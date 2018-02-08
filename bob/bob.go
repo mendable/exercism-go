4c8d9073d2f26510c4cf7f76e0b007114567a889
Jason Lambert <mendable@users.noreply.github.com>
2018-02-08 11:49:00 -0500

    Solution for bob

--- bob/bob.go
+++ bob/bob.go
@@ -1,15 +1,31 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package bob should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Bob is a lackadaisical teenager. In conversation, his responses are very limited.
 package bob
 
-// Hey should have a comment documenting it.
+import "strings"
+
+// Talk to Bob through through the Hey function, passing a remark as a string.
+// Returns a string response.
+//
+// Bob answers 'Sure.' if you ask him a question. (question mark)
+// He answers 'Whoa, chill out!' if you yell at him. (uppercase)
+// He answers 'Calm down, I know what I'm doing!' if you yell a question at him. (question mark + uppercase)
+// He says 'Fine. Be that way!' if you address him without actually saying anything. (empty string, spaces, tabs etc)
+// He answers 'Whatever.' to anything else.
 func Hey(remark string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	trimmedRemark := strings.TrimSpace(remark)
+
+	isQuestion := strings.HasSuffix(trimmedRemark, "?")
+	isYell := (strings.ToUpper(remark) == remark) && !(strings.ToLower(remark) == remark)
+
+	if isYell && isQuestion {
+		return "Calm down, I know what I'm doing!"
+	} else if isYell {
+		return "Whoa, chill out!"
+	} else if isQuestion {
+		return "Sure."
+	} else if len(trimmedRemark) == 0 {
+		return "Fine. Be that way!"
+	} else {
+		return "Whatever."
+	}
 }
